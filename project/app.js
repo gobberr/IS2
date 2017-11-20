a0b3024fba43a30ab3276e47b163c1eb4cbfcc6b
Luca <luke.fregolon@gmail.com>
2017-11-20 18:16:16 +0100

    Added html files and fixed app.js and route.js

--- project/app.js
+++ project/app.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var route = require('./routes/route');
-app.set('public', path.join(__dirname, 'public'));
-app.use('/', route);
+app.use(express.static(path.join(__dirname, 'public/style')));
+app.use(express.static(path.join(__dirname, 'public/img')));
 
+app.use('/', route);
 app.get('*', function(req, res){
-    res.send('404');
+    res.sendFile("404.html", { root : "public"})
   });
 app.listen(3000);
 
--- project/routes/route.js
+++ project/routes/route.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", function(req,res){ res.sendFile("index.html", {root: req.app.get("public")})});
 
-router.get("/cerca", function(req,res){res.sendFile("cerco_ripetizioni.html",{ root : req.app.get("public")})});
+router.get("/", function(req,res){ res.sendFile("index.html", {root: "public"})});
+
+router.get("/cerca", function(req,res){res.sendFile("cerco_ripetizioni.html",{ root : "public"})});
+
+router.get("/login", function(req,res){res.sendFile("login.html",{ root : "public"})});
+
+router.get("/registrati", function(req,res){res.sendFile("registrati.html",{ root : "public"})});
+
+router.get("/offri", function(req,res){res.sendFile("offro_ripetizioni.html",{ root : "public"})});
 
 
 module.exports = router;
